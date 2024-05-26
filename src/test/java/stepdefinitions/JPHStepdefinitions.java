f29a7b34ab177df522e9aae269a14d60f386b44e
bulutluoz <abbulutluoz@gmail.com>
2024-05-26 20:38:26 +0200

    4.gun sonu

--- src/test/java/stepdefinitions/JPHStepdefinitions.java
+++ src/test/java/stepdefinitions/JPHStepdefinitions.java
@@ -2,9 +2,12 @@ package stepdefinitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.junit.Assert;
 import utilities.ConfigReader;
 
@@ -15,6 +18,9 @@ public class JPHStepdefinitions {
     public String endpoint="";
     Response actualResponse;
     JsonPath actualResponseJpath;
+    JSONObject postRequestBody;
+    static JSONObject putRequestBody;
+    JSONObject expectedResponseBody;
 
     @Given("Kullanici {string} base URL'ini kullanir")
     public void kullanici_base_url_ini_kullanir(String configdekiBaseUrl) {
@@ -56,5 +62,113 @@ public class JPHStepdefinitions {
     @Then("jPH GET response body'sinde {string} degerinin String {string}")
     public void j_ph_get_response_body_sinde_degerinin_string(String verilenAttribute, String strAtrValue) {
         Assert.assertEquals(strAtrValue,actualResponseJpath.getString(verilenAttribute));
+    }
+
+    @Then("POST request icin {string},{string},{int} bilgileri ile request body olusturur")
+    public void post_request_icin_bilgileri_ile_request_body_olusturur(String title, String body, Integer userId) {
+
+        postRequestBody = new JSONObject();
+        postRequestBody.put("title",title);
+        postRequestBody.put("body",body);
+        postRequestBody.put("userId",userId);
+    }
+    @Then("jPH server a POST request gonderir ve testleri yapmak icin response degerini kaydeder")
+    public void j_ph_server_a_post_request_gonderir_ve_testleri_yapmak_icin_response_degerini_kaydeder() {
+
+        actualResponse = RestAssured
+                                .given().contentType(ContentType.JSON)
+                                .when().body(postRequestBody.toString())
+                                .post(endpoint);
+    }
+    @Then("jPH respons daki {string} header degerinin {string}")
+    public void j_ph_respons_daki_header_degerinin(String verilenHeader, String expectedHeaderDegeri) {
+
+        actualResponse
+                .then()
+                .assertThat()
+                .header(verilenHeader,expectedHeaderDegeri);
+    }
+    @Then("response attribute degerlerinin {string},{string},{int} ve {int} oldugunu test eder")
+    public void response_attribute_degerlerinin_ve_oldugunu_test_eder(String expectedTitle, String expectedBody, Integer expectedUserId, Integer expectedId) {
+
+        /*
+        {
+            "title": "Ahmet",
+            "body": "Merhaba",
+            "userId": 10,
+            "id": 101
+        }
+         */
+
+        // Elimizde response var
+        // response objesinden attribute degerlerini almak icin
+        // Jsonpath'e cevirmeliyiz
+
+        actualResponseJpath = actualResponse.jsonPath();
+
+        Assert.assertEquals(expectedTitle,actualResponseJpath.getString("title"));
+        Assert.assertEquals(expectedBody,actualResponseJpath.getString("body"));
+        Assert.assertEquals(expectedUserId,(Integer)actualResponseJpath.getInt("userId"));
+        Assert.assertEquals(expectedId,(Integer)actualResponseJpath.getInt("id"));
+
+
+    }
+
+    @When("PUT request icin {string} {string} ve {int} degerleri ile request body olusturur")
+    public void put_request_icin_ve_degerleri_ile_request_body_olusturur(String title, String body, Integer userId) {
+        putRequestBody = new JSONObject();
+        putRequestBody.put("title",title);
+        putRequestBody.put("body",body);
+        putRequestBody.put("userId",userId);
+    }
+    @When("Test icin  {string} {string} {int} ve {int} degerleri ile expected response body olusturur")
+    public void test_icin_ve_degerleri_ile_expected_response_body_olusturur(String expectedTitle, String expectedBody, Integer expectedUserId, Integer expectedId) {
+        //{
+        //    "title": "Son Test",
+        //    "body": "API veda",
+        //    "userId": 10,
+        //    "id": 77
+        //}
+        expectedResponseBody = new JSONObject();
+        expectedResponseBody.put("title",expectedTitle);
+        expectedResponseBody.put("body",expectedBody);
+        expectedResponseBody.put("userId",expectedUserId);
+        expectedResponseBody.put("id",expectedId);
+    }
+    @Then("jPH server'a PUT request gonderir ve response'i kaydeder")
+    public void j_ph_server_a_put_request_gonderir_ve_response_i_kaydeder() {
+        actualResponse = RestAssured
+                                    .given().contentType(ContentType.JSON)
+                                    .when().body(putRequestBody.toString())
+                                    .put(endpoint);
+
+    }
+    @Then("expected response body ile actual response'daki attribute degerlerinin ayni oldugunu test eder")
+    public void expected_response_body_ile_actual_response_daki_attribute_degerlerinin_ayni_oldugunu_test_eder() {
+        // expectedResponseBody (JSONObject) <==> actualResponse (Jsonpath'e cevirecegiz)
+
+        actualResponseJpath = actualResponse.jsonPath();
+
+        Assert.assertEquals(
+                expectedResponseBody.getString("title"),
+                actualResponseJpath.getString("title")
+                );
+
+        Assert.assertEquals(
+                expectedResponseBody.getString("body"),
+                actualResponseJpath.getString("body")
+        );
+
+        Assert.assertEquals(
+                expectedResponseBody.getInt("userId"),
+                actualResponseJpath.getInt("userId")
+        );
+
+        Assert.assertEquals(
+                expectedResponseBody.getInt("id"),
+                actualResponseJpath.getInt("id")
+        );
+
+
     }
 }
---
+++ src/test/resources/features/US1002_JPH_PostRequest.feature
@@ -0,0 +1,15 @@
+
+    Feature: US1002 kullanici JPH da post request ile test yapar
+
+
+      Scenario: TC02 Post request
+
+        Given Kullanici "jPHBaseUrl" base URL'ini kullanir
+        Then Path parametreleri icin "posts" kullanir
+        And POST request icin "Mehmet","Merhaba",10 bilgileri ile request body olusturur
+        And jPH server a POST request gonderir ve testleri yapmak icin response degerini kaydeder
+        Then jPH response'da status degerinin 201
+        And jPH response'da content type degerinin "application/json; charset=utf-8"
+        And jPH respons daki "Connection" header degerinin "keep-alive"
+        And jPH responseBody'sindeki attributeleri test etmek icin response i JsonPath objesine cast eder
+        Then response attribute degerlerinin "Mehmet","Merhaba",10 ve 101 oldugunu test eder
